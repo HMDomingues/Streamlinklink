52c169ae4c6fc2b5a6b907f97529b8f6469ebda0
Hugo <hugomdomingues@gmail.com>
2021-12-06 00:04:02 +0000

    Add files via upload

---
+++ background.js
@@ -0,0 +1,27 @@
+
+
+browser.contextMenus.create({
+    id: "run-in-streamlink",
+    title: "Run in Streamlink",
+    contexts: ["link"]
+});
+
+
+nativeName = "streamlinklink";
+
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case "run-in-streamlink":
+      url = info.linkUrl;
+
+      break;
+    case "streamlink-page":
+      url = info.pageUrl;
+      break;
+  }
+  if(url !== undefined && url !== null){
+    console.log(url);
+    s = browser.runtime.sendNativeMessage(nativeName, url);
+  }
+});
